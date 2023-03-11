4a4531b7a978cf8cfd4c09e09e9cb57b9eea4e7a
Hazuli Fidastian <hazulifidastian@gmail.com>
2023-03-11 21:59:23 +0700

    Request parsed

--- src/main.zig
+++ src/main.zig
@@ -2,20 +2,49 @@ const std = @import("std");
 const net = std.net;
 const StreamServer = net.StreamServer;
 const Address = net.Address;
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
 pub const io_mode = .evented;
 
 pub fn main() !void {
+    var gpa = GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     var stream_server = StreamServer.init(.{});
     const address = try Address.parseIp4("127.0.0.1", 8080);
     try stream_server.listen(address);
 
     while(true) {
         const connection = try stream_server.accept();
-        try handler(connection.stream);
+        try handler(allocator, connection.stream);
     }
 }
 
-fn handler(stream: net.Stream) !void {
+fn handler(allocator: std.mem.Allocator, stream: net.Stream) !void {
     defer stream.close();
-    try stream.writer().print("Hello world from zig", .{});
+    var first_line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+    first_line = first_line[0..first_line.len];
+    var first_line_iter = std.mem.split(u8, first_line, " ");
+
+    const method = first_line_iter.next().?;
+    const uri = first_line_iter.next().?;
+    const version = first_line_iter.next().?;
+    var headers = std.StringHashMap([]const u8).init(allocator);
+
+    while(true) {
+        var line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+        if (line.len == 1 and std.mem.eql(u8, line, "\r")) break;
+        line = line[0..line.len];
+
+        var line_iter = std.mem.split(u8, line, ":");
+        const key = line_iter.next().?;
+        var value = line_iter.next().?;
+        if (value[0] == ' ') value = value[1..];
+        try headers.put(key, value);
+    }
+
+    std.debug.print("method: {s}\nuri: {s}\nversion: {s}\n", .{method, uri, version});
+    
+    var headers_iter = headers.iterator();
+    while(headers_iter.next()) |entry| {
+        std.debug.print("{s}: {s}\n", .{entry.key_ptr.*, entry.value_ptr.*});
+    }
 }
